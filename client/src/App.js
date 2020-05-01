bb205f2318dfe3330bd8e38f001c2d77f96697d1
BrentonHolloway <brenton.holloway@gmail.com>
2020-05-01 13:06:12 +1000

    Header, Sidebar and Footer complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 function App(props) {
+  const sidebarItems = [
+    {title: 'item 1', link: '/'},
+    {title: 'item 2', link: '/'}
+  ];
+
+  const headerItems = [
+    {title: 'Logout', onClick: props.logoutHandler},
+  ];
   return (
     <div className="App">
-      <Header logoutHandler={props.logoutHandler}/>
+      <Header title={process.env.REACT_APP_NAME} items={headerItems}/>
+      <main>
+        <Sidebar items={sidebarItems}/>
+      </main>
+      
+      <Footer messageLeft={process.env.REACT_APP_NAME} messageRight="Hello"/>
     </div>
   );
 }
