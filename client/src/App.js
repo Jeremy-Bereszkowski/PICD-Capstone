bf9e53e59e6f6a851c768698d0634e64169e98ac
s3539822 <s3539822@student.rmit.edu.au>
2020-05-07 20:10:23 +1000

    authRoute.js built

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,27 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react'
 import Header from './components/Header'
-import Dashboard from './components/Dashboard'
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
+import Router from './authRoutes'
+import Footer from './components/Footer'
+/* import Sidebar from './components/Sidebar'; */
 
 function App(props) {
   
-  const sidebarItems = [
+  /* const sidebarItems = [
     {title: 'item 1', link: '/'},
     {title: 'item 2', link: '/'}
   ];
-
+ */
   const headerItems = [
     {title: 'Logout', onClick: props.logoutHandler},
   ];
 
   return (
     <div className="App">
-      <Router>
-        <Header items={headerItems}/>
-        <main>
-          <Route exact path="/">
-            <Dashboard/>
-          </Route>
-          {/* <Route path="/project" component={Project}/> */}
-        </main>
+      <main>
+        <Header items={headerItems} />
+        <Router />
         <Footer />
-      </Router>
+      </main>
     </div>
   );
 }
