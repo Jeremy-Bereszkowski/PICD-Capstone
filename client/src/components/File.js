4429ce8a8e8b3ba124f2886259b03021bf6c504e
BrentonHolloway <brenton.holloway@gmail.com>
2020-09-04 16:59:41 +1000

    Fixed date time format of files

--- client/src/components/File.js
+++ client/src/components/File.js
@@ -1,7 +1,3 @@
-// import React, { Component } from 'react'
-// import axios from 'axios';
-// import download from 'js-file-download';
-
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 import download from 'js-file-download';
@@ -78,10 +74,19 @@ function File({projectId, stageId, stageVersion}) {
                      <tbody>
                          {
                              files.map((file, index) => {
+                                 const timestamp = new Date(file.created_at);
+                                 var formattedTimestamp = new Intl.DateTimeFormat('en-US',{
+                                    year: "numeric",
+                                    month: "short",
+                                    day: "2-digit",
+                                    hour: "numeric",
+                                    minute: "2-digit",
+                                    second: "2-digit"
+                                  }).format(timestamp);
                                  return (
                                      <tr key={file.file_id}>
                                          <td>{file.original_filename}</td>
-                                         <td className="text-right">{file.created_at}</td>
+                                         <td className="text-right">{formattedTimestamp}</td>
                                          <td className="text-right"><input type="button" value="Download" className="btn btn-primary" onClick={() => onIndividualDownloadHandler(file.file_id, file.original_filename)}/></td>
                                      </tr>
                                  )
@@ -95,121 +100,3 @@ function File({projectId, stageId, stageVersion}) {
 }
 
 export default File;
-
-
-
-// TODO: should be refactored as a functional component
-// class File extends Component {
-
-//     constructor(props) {
-//         super(props)
-    
-//         this.state = {
-//             projectId: null,
-//             stageId: null,
-//             stageVersion: null,
-//             files: []
-//         }
-//     }
-
-//     getStageFiles = (projectId, stageId, stageVersion) => {
-//         fetch(process.env.REACT_APP_API_SERVER_ADDRESS+'/media/'+projectId+'/'+stageId+'/'+stageVersion)
-//             .then(res => res.json())
-//             .then(files => {
-//                 this.setState({
-//                     files: files
-//                 })
-//             });
-//     }
-    
-//     componentDidUpdate() {
-//         console.log('Props => '+this.props.stageVersion);
-//         console.log('State => '+this.state.stageVersion);
-        
-//         if (this.state.projectId !== this.props.projectId || this.state.stageId !== this.props.stageId || this.state.stageVersion !== this.props.stageVersion) {
-//             this.getStageFiles(this.props.projectId, this.props.stageId, this.props.stageVersion);
-//             this.setState({
-//                 projectId: this.props.projectId,
-//                 stageId: this.props.stageId,
-//                 stageVersion: this.props.stageVersion
-//             })
-//         }
-//     }
-
-//     onIndividualDownloadHandler = (file_id, filename) => {
-//         /**
-//          * Valid Response Types
-//          * * arraybuffer
-//          * * blob
-//          * * document
-//          * * json
-//          * * stream
-//          * * text
-//          */
-//         const responseType = 'blob';
-//         axios({
-//             url: process.env.REACT_APP_API_SERVER_ADDRESS+'/media/download/file/'+file_id,
-//             method: 'GET',
-//             responseType: responseType,
-//         })
-//         .then(res => {
-//             download(res.data, filename, res.headers['content-type'].split(';')[0]);
-//         });
-//     }
-
-//     onDownloadAllHandler = () => {
-//         /**
-//          * Valid Response Types
-//          * * arraybuffer
-//          * * blob
-//          * * document
-//          * * json
-//          * * stream
-//          * * text
-//          */
-//         const responseType = 'blob';
-//         console.log('Download all');
-//         axios({
-//             url: process.env.REACT_APP_API_SERVER_ADDRESS+'/media/download/stage/'+this.state.stageId+'/'+this.state.stageVersion,
-//             method: 'GET',
-//             responseType: responseType,
-//         })
-//         .then(res => {
-//             console.log(res);
-//             download(res.data, 'stage.zip', res.headers['content-type'].split(';')[0]);
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="container">
-//                 <table>
-//                     <tbody>
-//                         <tr>
-//                             <td className="text-right"><input type="button" value="Download All" className="btn btn-primary" onClick={() => this.onDownloadAllHandler()}/></td>
-//                         </tr>
-//                     </tbody>
-//                 </table>
-//                 <table className="table table-hover">
-//                     <tbody>
-//                         {
-//                             this.state.files.map((file, index) => {
-//                                 return (
-//                                     <tr key={file.file_id}>
-//                                         <td>{file.original_filename}</td>
-//                                         <td className="text-right">{file.created_at}</td>
-//                                         <td className="text-right"><input type="button" value="Download" className="btn btn-primary" onClick={() => this.onIndividualDownloadHandler(file.file_id, file.original_filename)}/></td>
-//                                     </tr>
-//                                 )
-//                             })
-//                         }
-//                     </tbody>
-                    
-//                 </table>
-//             </div>
-            
-//         )
-//     }
-// }
-
-// export default File;
