9b8fb5f3d2cce8afe24dc7cbadcb977aff495337
BrentonHolloway <brenton.holloway@gmail.com>
2020-09-05 18:32:29 +1000

    File Version History Complete

--- client/src/components/File.js
+++ client/src/components/File.js
@@ -1,16 +1,23 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import download from 'js-file-download';
+import FileHistoryModal from './FileHistoryModal';
 
-function File({projectId, stageId, stageVersion}) {
+function File({ projectId, stageId, stageVersion }) {
     const [files, setFiles] = useState([]);
+
+    /**
+     * Modal Data
+     */
+    const [showFileHistory, setShowFileHistory] = useState(false);
+    const [fileGroup, setFileGroup] = useState([]);
 
     /**
      * Get the files for the stage's version.
      * This function is run every time one of the props are changed.
      */
     useEffect(() => {
-        fetch(process.env.REACT_APP_API_SERVER_ADDRESS+'/media/'+projectId+'/'+stageId+'/'+stageVersion)
+        fetch(process.env.REACT_APP_API_SERVER_ADDRESS + '/media/' + projectId + '/' + stageId + '/' + stageVersion)
             .then(res => res.json())
             .then(files => {
                 setFiles(files);
@@ -29,13 +36,13 @@ function File({projectId, stageId, stageVersion}) {
          */
         const responseType = 'blob';
         axios({
-            url: process.env.REACT_APP_API_SERVER_ADDRESS+'/media/download/file/'+file_id,
+            url: process.env.REACT_APP_API_SERVER_ADDRESS + '/media/download/file/' + file_id,
             method: 'GET',
             responseType: responseType,
         })
-        .then(res => {
-            download(res.data, filename, res.headers['content-type'].split(';')[0]);
-        });
+            .then(res => {
+                download(res.data, filename, res.headers['content-type'].split(';')[0]);
+            });
     }
 
     const onDownloadAllHandler = () => {
@@ -51,56 +58,81 @@ function File({projectId, stageId, stageVersion}) {
         const responseType = 'blob';
         console.log('Download all');
         axios({
-            url: process.env.REACT_APP_API_SERVER_ADDRESS+'/media/download/stage/'+stageId+'/'+stageVersion,
+            url: process.env.REACT_APP_API_SERVER_ADDRESS + '/media/download/stage/' + stageId + '/' + stageVersion,
             method: 'GET',
             responseType: responseType,
         })
-        .then(res => {
-            console.log(res);
-            download(res.data, 'stage.zip', res.headers['content-type'].split(';')[0]);
-        });
+            .then(res => {
+                console.log(res);
+                download(res.data, 'stage.zip', res.headers['content-type'].split(';')[0]);
+            });
+    }
+
+    /**
+     * Groups files by original filename
+     */
+    const fileGroups = Array.from(new Set(files.map(f => f.original_filename)))
+        .map(filename => {
+            return files.filter(file => file.original_filename === filename);
+        })
+
+
+    const showHistory = (files, index) => {
+        setFileGroup(files);
+        setShowFileHistory(true);
+    }
+
+    const hideHistory = () => {
+        setShowFileHistory(false);
+        setFileGroup([]);
     }
 
     return (
         <>
-        {
-            files.length === 0 ?
-                <div className="col text-center">
-                    <h5 className="pt-2">No Files have been Uploaded</h5>
-                    <p>Change Version to see files of other versions</p>
-                </div>
-            :
-            <div className="container">
-                <div className="col text-right py-2">
-                    <input type="button" value="Download All" className="btn btn-primary" onClick={() => onDownloadAllHandler()}/>
-                </div>
-                <table className="table table-hover">
-                    <tbody>
-                        {
-                            files.map((file, index) => {
-                                const timestamp = new Date(file.created_at);
-                                var formattedTimestamp = new Intl.DateTimeFormat('en-AU',{
-                                year: "numeric",
-                                month: "short",
-                                day: "2-digit",
-                                hour: "numeric",
-                                minute: "2-digit",
-                                second: "2-digit"
-                                }).format(timestamp);
-                                return (
-                                    <tr key={file.file_id}>
-                                        <td>{file.original_filename}</td>
-                                        <td className="text-right">{formattedTimestamp}</td>
-                                        <td className="text-right"><input type="button" value="Download" className="btn btn-primary" onClick={() => onIndividualDownloadHandler(file.file_id, file.original_filename)}/></td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                
-                </table>
-            </div>
-        }
+            {
+                files.length === 0 ?
+                    <div className="container">
+                        <div className="col text-center">
+                            <h5 className="pt-2">No Files have been Uploaded</h5>
+                            <p>Change Version to see files of other versions</p>
+                        </div>
+                    </div>
+                    :
+                    <div className="container" id="files">
+                        <div className="col text-right py-2">
+                            <input type="button" value="Download All" className="btn btn-primary" onClick={() => onDownloadAllHandler()} />
+                        </div>
+                        <table className="table table-hover">
+                            <tbody>
+                                {
+                                    fileGroups.map((group, index) => {
+                                        var files = group.sort((a, b) => new Date(a.updated_at) < new Date(b.updated_at) ? 1 : -1);
+                                        var file = files[0];
+                                        const timestamp = new Date(file.updated_at);
+                                        var formattedTimestamp = new Intl.DateTimeFormat('en-AU', {
+                                            year: "numeric",
+                                            month: "short",
+                                            day: "2-digit",
+                                            hour: "numeric",
+                                            minute: "2-digit",
+                                            second: "2-digit"
+                                        }).format(timestamp);
+                                        return (
+                                            <tr key={file.file_id} onClick={() => showHistory(files, index)}>
+                                                <td>{file.original_filename}</td>
+                                                <td className="text-right">{'v' + files.length}</td>
+                                                <td className="text-right">{formattedTimestamp}</td>
+                                                <td className="text-right"><input type="button" value="Download" className="btn btn-primary" onClick={() => onIndividualDownloadHandler(file.file_id, file.original_filename)} /></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        <FileHistoryModal files={fileGroup} show={showFileHistory} hide={() => hideHistory()} handleDownload={onIndividualDownloadHandler}/>
+                    </div>
+            }
+
         </>
     )
 }
