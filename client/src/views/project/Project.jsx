f50f0f4ee5e1b5672d97b7715412dd837763c75f
BrentonHolloway <brenton.holloway@gmail.com>
2020-09-07 16:33:35 +1000

    Updated Overview Page

--- client/src/views/project/Project.jsx
+++ client/src/views/project/Project.jsx
@@ -1,72 +1,102 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Sidebar from '../../components/Sidebar'
 
-class Project extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            project_id: "",
-            title: "",
-            date_stamp: ""
-        }
-    }
+// TODO: Add project owner and contributors
+// TODO: Updated at needs to be updated when something in the project updates
+const Project = (props) => {
+    const projectID = props.match.params.projectId;
 
-    getProjectData(projectID) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("")
+    const [createdAt, setCreatedAt] = useState("");
+    const [updatedAt, setUpdatedAt] = useState("");
+
+    /**
+     * Get project details on mount
+     */
+    useEffect(() => {
         fetch(process.env.REACT_APP_API_SERVER_ADDRESS+"/project/"+projectID)
         .then(res => res.json())
         .then(res => {
-            this.setState({
-                project_id: res.project.project_id,
-                title: res.project.title,
-                date_stamp: res.project.date_stamp
-            })
+            setTitle(res.project.title);
+            setDescription(res.project.description);
+
+            const created = new Date(res.project.created_at);
+            const updated = new Date(res.project.updated_at);
+
+            /**
+             * Time Region for formatting
+             */
+            const region = 'en-AU';
+
+            /**
+             * Time Formatting Options
+             */
+            const timeFormatOptions = {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+                hour: "numeric",
+                minute: "2-digit",
+                second: "2-digit"
+            };
+
+            var createdFormatted = new Intl.DateTimeFormat(region, timeFormatOptions).format(created);
+            var updatedFormatted = new Intl.DateTimeFormat(region, timeFormatOptions).format(updated);
+
+            setCreatedAt(createdFormatted);
+            setUpdatedAt(updatedFormatted);
         });
-    }
+    }, [projectID])
 
-    componentDidMount() {
-        this.getProjectData(this.props.match.params.projectId);
-    }
-
-    handleFormChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit = (event) => {
-        fetch(process.env.REACT_APP_API_SERVER_ADDRESS+'/project/'+this.state.project_id+'/update', {
-            method: 'post',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                title: this.state.title
-            })
-        }).then((res) => {
-            this.componentWillMount()
-        })
-        event.preventDefault()
-    }
-
-    render() {
-        return (
+    return (
+        <div>
             <div className="row justify-content-left content">
-                <Sidebar id={this.props.match.params.projectId}/>
+                <Sidebar id={props.match.params.projectId}/>
                 <div className="col">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-md-6">
                             <h3>
-                            {this.state.title}
+                            {title}
                             </h3>
                         </div>
                     </div>
                     <hr/>
-                    
+
+                    <div className="row">
+                        <div className="col-md-5">
+                            <h5>
+                                Description
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="row">
+                        <div className="col">
+                            <div className="form-group row">
+                                <label htmlFor="updatedAt" className="col-md-2 col-form-label text-md-right">Updated at: </label>
+                                <div className="col-md-6 form-inline">
+                                    {updatedAt}
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label htmlFor="createdAt" className="col-md-2 col-form-label text-md-right">Created at: </label>
+                                <div className="col-md-6 form-inline">
+                                    {createdAt}
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Project
